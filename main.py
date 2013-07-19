004907c939ed2bc6eea1bc10844f3084d50be2aa
Bengi Mizrahi <bengimizrahi@gmail.com>
2013-07-19 15:24:03 +0300

    version 3

--- main.py
+++ main.py
@@ -5,111 +5,241 @@ class InputParser:
     def __init__(self, inputFile, game):
 	self.game = game
 	self.inputFile = inputFile
+	self.errors = []
+	self.warnings = []
 
     def parse(self):
 	input = open(self.inputFile, 'r')
-	for line in input.readlines():
+	for (i, line) in enumerate(input.xreadlines()):
+	    self.lineNumber = i + 1
 	    line = line.strip()
 	    if len(line) == 0 or line.startswith('#'): continue
 	    line = line.lower()
 	    args = line.split(' ')
 	    cmd = args.pop(0)
-	    self.readLine(cmd, args)
+	    if self.readLine(cmd, args) == False:
+		return False
 	input.close()
+	return True
+
+    def isNumber(self, str):
+	tokens = str.split(".")
+	for t in tokens:
+	    if not t.isdigit():	
+		return False
+	return True
 
     def readLine(self, cmd, args):
 	if cmd == "num-seats":
+	    if not self.isNumber(args[0]):
+		self.errors.append((self.lineNumber, "%s is not a valid number" % (args[0])))
+		return False
 	    numSeats = int(args.pop(0))
-	    game.setNumSeats(numSeats)
-	    return True
+	    if numSeats < 2 or numSeats > 10:
+		self.errors.append((self.lineNumber, "Number of seats cannot be less than 2 or greater than 10"))
+		return False
+	    else:
+		self.game.setNumSeats(numSeats)
+		return True
 	elif cmd == "blinds":
+	    if len(args) != 2:
+		self.errors.append((self.lineNumber, "You should put two entries, one for small blind and one for big blind"))
+		return False
+	    if not self.isNumber(args[0]):
+		self.errors.append((self.lineNumber, "%s is not a valid number" % (args[0])))
+	    if not self.isNumber(args[1]):
+		self.errors.append((self.lineNumber, "%s is not a valid number" % (args[1])))
 	    smallBlind = int(args.pop(0))
 	    bigBlind = int(args.pop(0))
-	    game.setBlinds(smallBlind, bigBlind)
+	    if smallBlind > bigBlind:
+		self.errors.append((self.lineNumber, "Small blind should less than big blind"))
+		return False
+	    self.game.setBlinds(smallBlind, bigBlind)
 	    return True
 	elif cmd == "antes":
+	    if len(args) != 1:
+		self.warnings.append((self.lineNumber, "There must be only one entry for antes"))
+	    if not self.isNumber(args[0]):
+		self.errors.append((self.lineNumber, "%s is not a valid number" % (args[0])))
 	    antes = int(args.pop(0))
-	    game.setAntes(antes)
+	    self.game.setAntes(antes)
 	    return True
 	elif cmd == "player":
+	    if not self.isNumber(args[0]):
+		self.errors.append((self.lineNumber, "%s is not a valid number" % (args[0])))
+	    if not self.isNumber(args[0]):
+		self.errors.append((self.lineNumber, "%s is not a valid number" % (args[0])))
 	    playerIndex = int(args.pop(0))
-	    return game.insertPlayerAtIndex(playerIndex)
+	    if self.game.insertPlayerAtIndex(playerIndex):
+		return True
+	    else:
+		self.errors.append((self.lineNumber, "Failed to select player with index %s" % (playerIndex)))
+		return False
 	elif cmd == "name":
-	    player = game.cursor
+	    player = self.game.currentPlayer
 	    if player:
+		if len(args) == 0:
+		    self.warnings.append((self.lineNumber, "No name has been entered here"))
+		    name = "?"
+		elif len(args) > 1:
+		    self.warnings.append((self.lineNumber, "Only one name is allowed here"))
 		name = args.pop(0)
 		player.setName(name)
 		return True
 	    else:
+		self.errors.append((self.lineNumber, "You should first select a player before naming"))
 		return False
 	elif cmd == "chips":
-	    player = game.cursor
+	    player = self.game.currentPlayer
 	    if player:
+		if len(args) == 0:
+		    self.errors.append((self.lineNumber, "No amount of chips have been entered here"))
+		    return False
+		elif len(args) > 1:
+		    self.warnings.append((self.lineNumber, "Only one entry is allowed here"))
+		if not self.isNumber(args[0]):
+		    self.errors.append((self.lineNumber, "%s is not a valid number" % (args[0])))
 		chips = int(args.pop(0))
 		player.setChips(chips)
 		return True
 	    else:
+		self.errors.append((self.lineNumber, "You should first select a player before giving chips"))
 		return False
 	elif cmd == "cards" or cmd == "hold" or cmd == "holds":
-	    player = game.cursor
+	    player = self.game.currentPlayer
 	    if player:
 		player.setCards(args)
 		return True
 	    else:
+		self.errors.append((self.lineNumber, "You should first select a player before selecting cards"))
 		return False
 	elif cmd == "dealer":
 	    if len(args) == 0:
-		player = game.cursor
+		player = self.game.currentPlayer
 		if player:
-		    game.setDealer(player)
+		    self.game.setDealer(player)
 		    return True
 		else:
+		    self.errors.append((self.lineNumber, "Either select a player first, or append the player name here"))
 		    return False
-	    elif len(args) == 1:
-		name = args.pop(0)
-		player = game.playerByName(name)
-		if player:
-		    game.setDealer(player)
-		    return True
-		else:
-		    return False
+	    if len(args) > 1:
+		self.warnings.append((self.lineNumber, "You entered multiple names here; only using the first one"))
+	    name = args.pop(0)
+	    player = self.game.playerByName(name)
+	    if player:
+		self.game.setDealer(player)
+		return True
+	    else:
+		self.errors.append((self.lineNumber, "Player %s cannot be found in the current list of players" % (name)))
+		return False
 	elif cmd == "community":
 	    if args[0] == "cards":
 		communityCards = args[1:]
 	    else:
 		communityCards = args
-	    return game.setCommunityCards(communityCards)
+	    if self.game.setCommunityCards(communityCards):
+		return True
+	    else:
+		self.errors.append((self.lineNumber, self.game.lastError))
+		return False
 	else:
 	    name = cmd
-	    player = game.playerByName(name)
+	    player = self.game.playerByName(name)
 	    if player:
+		if len(args) == 0:
+		    self.errors.append((lineNumber, "No actions has been entered here"))
+		    return False
 		action = args.pop(0)
 		if action == "hold" or action == "holds":
-		    player.setCards(args)
-		    return True
-		elif action == "bet" or action == "bets":
-		    # ACTION: bet
-		    return True
-		elif action == "call" or action == "calls":
-		    # ACTION: call
-		    return True
-		elif action == "raise" or action == "raises":
-		    # ACTION: raise
-		    return True
-		elif action == "go" or action == "goes":
-		    if args[0] == "all-in" or (args[0] == "all" and args[1] == "in"):
-			# ACTION: all-in
+		    lastArg = args[len(args)-1]
+		    if lastArg == "visible" or lastArg == "(visible)" or lastArg == "always-visible" or lastArg == "(always-visible)" or lastArg == "visible-always" or lastArg == "(visible-always)":
+			player.cardVisibility = ALWAYS_VISIBLE
+			args.pop()
+		    elif lastArg == "invisible" or lastArg == "(invisible)":
+			args.pop()
+		    elif lastArg == "visible-on-hover" or lastArg == "(visible-on-hover)":
+			player.cardVisibility = VISIBLE_ON_HOVER
+		    if player.setCards(args):
 			return True
 		    else:
+			self.errors.append((self.lineNumber, player.lastError))
 			return False
+		elif action == "fold" or action == "folds":
+		    if self.game.addAction(FOLD):
+			return True
+		    else:
+			self.errors.append((self.lineNumber, self.game.lastError))
+			return False
+		elif action == "bet" or action == "bets":
+		    if len(args) == 0:
+			self.errors.append((self.lineNumber, "No amount of bet has been entered here"))
+			return False
+		    if not self.isNumber(args[0]):
+			self.errors.append((self.lineNumber, "%s is not a valid number" % (args[0])))
+		    amount = int(args.pop(0))
+		    if self.game.addAction(BET, amount):
+			return True
+		    else:
+			self.errors.append((self.lineNumber, self.game.lastError))
+			return False
+		elif action == "call" or action == "calls":
+		    if self.game.addAction(CALL):
+			return True
+		    else:
+			self.errors.append((self.lineNumber, self.game.lastError))
+			return False
+		elif action == "raise" or action == "raises":
+		    nextWord = args[0]
+		    if nextWord == "to":
+			args.pop(0)
+		    if len(args) == 0:
+			self.errors.append((self.lineNumber, "No amount of raise has been entered here"))
+			return False
+		    if len(args) > 1:
+			self.warnings.append((self.lineNumber, "Too many numbers have been entered; only the first one will be used"))
+			return False
+		    if not self.isNumber(args[0]):
+			self.errors.append((self.lineNumber, "%s is not a valid number" % (args[0])))
+			return False
+		    amount = int(args.pop(0))
+		    if self.game.addAction(RAISE, amount):
+			return True
+		    else:
+			self.errors.append((self.lineNumber, "Error while processing action 'raise':\n\t%s" % (self.game.lastError)))
+			return False
+		elif action == "go" or action == "goes":
+		    if args[0] == "all-in" or (args[0] == "all" and args[1] == "in"):
+			if self.game.addAction(GO_ALL_IN):
+			    return True
+			else:
+			    self.errors.append((self.lineNumber, self.game.lastError))
+			    return False
+		    else:
+			self.errors.append((self.lineNumber, "Unknown command 'go'; did you mean 'go all-in'?"))
+			return False
+		else:
+		    self.errors.append((self.lineNumber, "Unknown action '%s'" % (action)))
+		    return False
 	    else:
-		return False
+		self.warnings.append((self.lineNumber, "Unknown command '%s'" % (cmd)))
+		return True
 
 
 if __name__ == "__main__":
     game = Game()
     parser = InputParser("input.txt", game)
-    parser.parse()
-    game.setup()
-    print game
+    if parser.parse():
+	print "Game:"
+	print game
+    else:
+	print "There were some errors in the text:"
+	if len(parser.errors) > 0:
+	    print "Errors:"
+	for (lineNumber, error) in parser.errors:
+	    print "On line %s: %s" % (lineNumber, error)
+	print
+	if len(parser.warnings) > 0:
+	    print "Warnings:"
+	for (lineNumber, warning) in parser.warnings:
+	    print "On line %s: %s" % (lineNumber, warning)
 
--- poker.py
+++ poker.py
@@ -1,9 +1,16 @@
+INVISIBLE, ALWAYS_VISIBLE, VISIBLE_ON_HOVER = range(3)
+NONE = 0
+CALL, BET, RAISE, FOLD, GO_ALL_IN = range(5)
+PRE_FLOP, FLOP, TURN, RIVER = range(4)
+
 class Player:
 
     def __init__(self):
 	self.name = ''
 	self.chips = 0
-	self.pot = 0
+	self.betOrRaise = 0
+	self.cardVisibility = INVISIBLE 
+	self.action = NONE
 
     def setName(self, name):
 	self.name = name
@@ -13,43 +20,26 @@ class Player:
 
     def setCards(self, cards):
 	self.cards = cards
+	return True
 
     def putMoney(self, amount):
-	assert self.chips >= amount
+	if self.chips >= amount:
+	    self.lastError = "Player %s has got only $%s worth of chips at this state"
+	    return False
 	self.chips -= amount
 	self.pot += amount
+	return True
 
     def __repr__(self):
-	return "Player(Name:%s, Chips:$%s, Pot:$%s)" % (self.name, self.chips, self.pot)
-
-
-class Action:
-    
-    def __init__(self, type, amount=0):
-	self.type = type
-	self.amount = amount
-
-    def __repr__(self):
-	return "Action(%s, $%s)" % (self.type, self.amount)
-
-
-class SidePot:
-    
-    def __def__(self):
-	self.amount = 0
-	self.players = []
-
-    def addAmount(self, amount):
-	self.amount += amount
-
-    def addPlayer(self, player):
-	self.players.append(player)
+	return "Player(Name:%s, Chips:$%s, Bet/Raise:$%s)" % (self.name, self.chips, self.betOrRaise)
 
 
 class Game:
 
     def __init__(self):
 	self.currentPlayer = None
+	self.started = False
+	self.stage = NONE
 
     def setNumSeats(self, numSeats):
 	self.players = [None] * numSeats
@@ -76,13 +66,16 @@ class Game:
 	self.currentPlayer = currentPlayer
 
     def insertPlayerAtIndex(self, index):
-	assert index > 0 and index <= 10
-	if index >= len(self.players):
-	    extendAmount = index - len(self.players) + 1
-	    self.players.extend([None] * extendAmount)
+	if index < 1:
+	    self.lastError = "Index number cannot be smaller than 1"
+	    return False
+	if index > len(self.players):
+	    self.lastError = "Index number cannot be greater than the number of players in table"
+	    return False
 	if self.players[index] == None:
 	    self.players[index] = Player()
-	self.cursor = self.players[index]
+	self.currentPlayer = self.players[index]
+	return True
 
     def setDealer(self, dealer):
 	self.dealer = dealer
@@ -96,6 +89,7 @@ class Game:
 
     def setCommunityCards(self, communityCards):
 	self.communityCards = communityCards
+	return True
 
     def playerByName(self, name):
 	for player in self.players:
@@ -104,26 +98,67 @@ class Game:
 		    return player
 	return None
 
-    def setup(self):
-	assert self.numPlayers() > 1
-	assert self.dealer
+    def start(self):
+	if self.numPlayers() <= 1:
+	    self.lastError = "There should be more than one player to start the game"
+	    self.started = False
+	    return False
+	if not self.dealer:
+	    self.lastError = "Cannot start the game without a dealer"
+	    self.started = False
+	    return False
 	if self.numPlayers() == 2:
 	    self.currentPlayer = self.dealer
 	    self.currentPlayer.putMoney(self.smallBlind)
 	    self.moveToNextPlayer()
 	    self.currentPlayer.putMoney(self.bigBlind)
+	    self.lastBetterOrRaiser = self.currentPlayer
 	    self.moveToNextPlayer()
 	else:
 	    self.currentPlayer = self.nextPlayer(self.dealer)
 	    self.currentPlayer.putMoney(self.smallBlind)
 	    self.moveToNextPlayer()
 	    self.currentPlayer.putMoney(self.bigBlind)
+	    self.lastBetterOrRaiser = self.currentPlayer
 	    self.moveToNextPlayer()
+	self.started = True
+	return True
 
+    def maxBetOrRaise(self):
+	return max(map(self.players, lambda x: x.betOrRaise))
+
+    def addAction(self, action, amount=0):
+	if not self.started:
+	    if not self.start():
+		self.lastError = "Could not start the game, the following error occurred:\n\t%s" % (self.lastError)
+		return False
+	if action == FOLD:
+	    self.currentPlayer.action = FOLD
+	    return True
+	elif action == CALL:
+	    return True
+	elif action == BET:
+	    if not self.currentPlayer.putMoney(amount):
+		self.lastError = self.currentPlayer.lastError
+		return False
+	    else:
+		return True
+	elif action == RAISE:
+	    if self.currentPlayer.putMoney(amount):
+		self.lastError = self.currentPlayer.lastError
+		return False
+	    else:
+		return True
+	elif action == GO_ALL_IN:
+	    # TODO
+	    return True
+	else:
+	    self.lastError = "Unknown action: %s" % (action)
+	    return False
     def __repr__(self):
 	return """Players: %s
 Blinds: %s-%s
 Antes: %s
 Dealer: %s
 Current: %s
-Community Cards: %s""" % (str(self.players), self.smallBlind, self.bigBlind, self.antes, self.dealer, self.cursor, str(self.communityCards)) 
+Community Cards: %s""" % (str(self.players), self.smallBlind, self.bigBlind, self.antes, self.dealer, self.currentPlayer, str(self.communityCards)) 
